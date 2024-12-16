6c46e344eb6fa0f0b3dcadc23bea413464f8c3a9
woollover-unix <diapostrofo@gmail.com>
2024-12-16 11:35:57 +0100

    initial sst setup

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "psm",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
